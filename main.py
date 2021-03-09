42972e13ccc2a35e37d7df945ea4c255080b2ad3
SA Romanchuk <gefest@me.com>
2021-03-09 22:05:29 +0300

    LES 7

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from gb_parse.spiders.autoyoula import AutoyoulaSpider
 from gb_parse.spiders.instagram import InstagramSpider
+from gb_parse.spiders.zillow import ZillowSpider
 
 
 if __name__ == "__main__":
@@ -12,10 +13,11 @@ if __name__ == "__main__":
     crawler_settings = Settings()
     crawler_settings.setmodule("gb_parse.settings")
     crawler_proc = CrawlerProcess(settings=crawler_settings)
-    crawler_proc.crawl(
-        InstagramSpider,
-        login=os.getenv("INST_LOGIN"),
-        password=os.getenv("INST_PASSWORD"),
-        tags=tags,
-    )
+    # crawler_proc.crawl(
+    #     InstagramSpider,
+    #     login=os.getenv("INST_LOGIN"),
+    #     password=os.getenv("INST_PASSWORD"),
+    #     tags=tags,
+    # )
+    crawler_proc.crawl(ZillowSpider)
     crawler_proc.start()
