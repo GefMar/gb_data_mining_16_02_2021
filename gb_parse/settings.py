ad5b1759d3b131067893056e65df2ddcd47c7aca
SA Romanchuk <gefest@me.com>
2021-03-09 20:29:09 +0300

    LES 6 HW

--- gb_parse/settings.py
+++ gb_parse/settings.py
@@ -13,7 +13,7 @@ SPIDER_MODULES = ["gb_parse.spiders"]
 NEWSPIDER_MODULE = "gb_parse.spiders"
 
 LOG_ENABLE = True
-LOG_LEVEL = "DEBUG"
+LOG_LEVEL = "INFO"
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:85.0) Gecko/20100101 Firefox/85.0"
 
