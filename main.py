8ede7f2ba44fd88e2467f0557395a6a2be7838d4
SA Romanchuk <gefest@me.com>
2021-03-12 20:12:26 +0300

    LES 7 HW

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     crawler_settings = Settings()
     crawler_settings.setmodule("gb_parse.settings")
     crawler_proc = CrawlerProcess(settings=crawler_settings)
-    # crawler_proc.crawl(
-    #     InstagramSpider,
-    #     login=os.getenv("INST_LOGIN"),
-    #     password=os.getenv("INST_PASSWORD"),
-    #     tags=tags,
-    # )
-    crawler_proc.crawl(ZillowSpider)
+    crawler_proc.crawl(
+        InstagramSpider,
+        login=os.getenv("INST_LOGIN"),
+        password=os.getenv("INST_PASSWORD"),
+        tags=tags,
+    )
+    # crawler_proc.crawl(ZillowSpider)
     crawler_proc.start()
